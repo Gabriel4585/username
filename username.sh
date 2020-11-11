43ad9bc323a14b7e7543d2a4eea4a62686446e8d
Gabriel Garcia <gagarcia@chapman.edu>
2020-11-11 11:03:45 -0800

    Finished script

--- username.sh
+++ username.sh
@@ -1,12 +1,12 @@
 #! /bin/bash
 # username.sh
 # Gabriel Garcia
-echo "Enter a five-digit ZIP code: "
-read ZIP
-while echo "$ZIP" | egrep -v "^[a-z][0-9]{5}$" > /dev/null 2>&1
+echo "Enter alphanumeric password between 3 and 12 characters long: "
+read password
+while echo "$password" | grep -E -v "^[a-z][a-z0-9_]{2,11}$" > /dev/null 2>&1
 do
-	echo "You must enter a valid ZIP code - five digits only!"
-	echo "Enter a five-digit ZIP code: "
-	read ZIP
+	echo "You must only enter between 3 and 12 digits and only numbers, lowercase letters, or _"
+	echo "Enter a password: "
+	read password
 done
 echo "Thank you"
